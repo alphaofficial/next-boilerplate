f1d69a757f6e24a5ca44e951661ac08434ce8964
Albert Alpha <baronofficial@gmail.com>
2020-05-12 15:08:26 +0000

    udpates

---
+++ components/HeadFile.js
@@ -0,0 +1,17 @@
+import Head from 'next/Head'
+
+export default function HeadFile(){
+
+    return(
+        <Head>
+            <meta charset="utf-8"/>
+            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+            <meta name="description" content=""/>
+            <meta name="keywords" content=""/>
+
+            <title>Mega Strore</title>
+            <link href="/assets/css/page.min.css" rel="stylesheet" />
+            <link href="/assets/css/style.css" rel="stylesheet" />
+        </Head>
+    )
+}
